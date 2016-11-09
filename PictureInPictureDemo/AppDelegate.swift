2485881a25b8d3915c2c3f1fcbd28eb367b82674
Prajakta Kulkarni <prajakta.kulkarni@mirumagency.com>
2016-11-09 13:00:52 +0530

    Completed demo for Picture in Picture for iPad

--- PictureInPictureDemo/AppDelegate.swift
+++ PictureInPictureDemo/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+        } catch {
+            print("Unable to set audio session category")
+        }
         return true
     }
 
--- PictureInPictureDemo/ViewController.swift
+++ PictureInPictureDemo/ViewController.swift
@@ -7,17 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVPlayerViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let webview:WKWebView = WKWebView(frame: self.view.frame)
+//        webview.load(URLRequest(url: URL(string: "https://www.youtube.com/watch?v=VC89P9GRFwQ")!))
+//        self.view.addSubview(webview)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as?
+        AVPlayerViewController
+        let url = Bundle.main.url(forResource: "videoplayback", withExtension: "mp4")
+            //NSURL(string:"https://youtu.be/raSbqN0mWZk")
+        
+        destination!.delegate = self
+        
+        if let movieURL = url {
+            destination!.player = AVPlayer(url: movieURL as URL)
+        }
+
+    }
+    func playerViewController(_ playerViewController: AVPlayerViewController,
+                              restoreUserInterfaceForPictureInPictureStopWithCompletionHandler
+        completionHandler: @escaping (Bool) -> Void) {
+        
+        let currentViewController = navigationController?.visibleViewController
+        
+        if currentViewController != playerViewController {
+            if let topViewController =
+                navigationController?.topViewController {
+                
+                topViewController.present(playerViewController,
+                                                        animated: true, completion: {()
+                                                            completionHandler(true)
+                })
+            }
+        }
     }
 
 
